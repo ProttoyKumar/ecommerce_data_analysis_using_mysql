22458b25b0d03eb18eb8eec474a8d34dba09f22e
Prottoy Kumar Prodhan Joy <80445079+ProttoyKumar@users.noreply.github.com>
2025-12-06 02:55:28 +0600

    Add files via upload

---
+++ analyzing_top_webpages.sql
@@ -0,0 +1,32 @@
+use mavenfuzzyfactory;
+
+-- top landing page
+CREATE TEMPORARY TABLE first_pageview
+SELECT 
+    website_session_id, MIN(website_pageview_id) as min_pv_id
+FROM
+    website_pageviews
+WHERE
+    website_pageview_id < 1000
+GROUP BY website_session_id
+
+SELECT 
+    website_pageviews.pageview_url,
+    COUNT(DISTINCT first_pageview.website_session_id)
+FROM
+    first_pageview
+        LEFT JOIN
+    website_pageviews ON first_pageview.min_pv_id = website_pageviews.website_pageview_id
+GROUP BY website_pageviews.pageview_url 
+
+
+-- top pages by pageviews
+SELECT 
+    pageview_url,
+    COUNT(DISTINCT website_pageview_id) AS count_pv
+FROM
+    website_pageviews
+WHERE
+    created_at < '2012-06-09'
+GROUP BY pageview_url
+ORDER BY count_pv DESC
