fb54a064206062054b437e351c510a1bdaec757d
Prottoy Kumar Prodhan Joy <80445079+ProttoyKumar@users.noreply.github.com>
2025-12-12 03:18:13 +0600

    Add files via upload

--- bounce_rate_analysis.sql
+++ bounce_rate_analysis.sql
@@ -52,3 +52,48 @@ FROM
     bounced_sessions_only ON bounced_sessions_only.website_session_id = sessions_w_landing_page_demo.website_session_id
 GROUP BY sessions_w_landing_page_demo.landing_page;
 
+-- Calculate bounce rate for the traffic landing on home page
+Create temporary table first_pageviews
+SELECT 
+    website_pageviews.website_session_id,
+    MIN(website_pageviews.website_pageview_id) AS min_pageview_id
+FROM
+    website_pageviews
+        Left JOIN
+    website_sessions ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE
+    website_sessions.created_at < '2012-06-14'
+GROUP BY website_pageviews.website_session_id;
+
+Create temporary table sessions_w_home_landing_page
+SELECT 
+    first_pageviews.website_session_id,
+    website_pageviews.pageview_url AS landing_page
+FROM
+    first_pageviews
+        Left JOIN
+    website_pageviews ON website_pageviews.website_pageview_id = first_pageviews.min_pageview_id
+WHERE
+    website_pageviews.pageview_url = '/home';
+
+Create temporary table bounced_sessions
+SELECT 
+    sessions_w_home_landing_page.website_session_id,
+    COUNT(DISTINCT website_pageviews.website_pageview_id) AS count_of_pages_viewed
+FROM
+    sessions_w_home_landing_page
+        Left JOIN
+    website_pageviews ON sessions_w_home_landing_page.website_session_id = website_pageviews.website_session_id
+GROUP BY sessions_w_home_landing_page.website_session_id
+HAVING COUNT(DISTINCT website_pageviews.website_pageview_id) = 1;
+
+SELECT 
+    COUNT(DISTINCT sessions_w_home_landing_page.website_session_id) AS sessions,
+    COUNT(DISTINCT bounced_sessions.website_session_id) AS bounced_sessions,
+    COUNT(DISTINCT bounced_sessions.website_session_id) / COUNT(DISTINCT sessions_w_home_landing_page.website_session_id) as bounce_rate
+FROM
+    sessions_w_home_landing_page
+        LEFT JOIN
+    bounced_sessions ON bounced_sessions.website_session_id = sessions_w_home_landing_page.website_session_id
+GROUP BY sessions_w_home_landing_page.landing_page;
+
